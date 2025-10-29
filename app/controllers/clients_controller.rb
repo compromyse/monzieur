fae6f67a425faa9916333e0f2e20021e5a236192
Raghuram Subramani <raghus2247@gmail.com>
2025-10-28 21:53:48 -0400

    controllers: clients: create

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,6 +5,21 @@ class ClientsController < ApplicationController
   end
 
   def create
-    debugger
+    client = Client.new(client_params)
+
+    if client.save
+      return redirect_to root_path, notice: 'Client Created!'
+    end
+
+    redirect_to root_path, alert: 'Unable to Create Client.'
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(
+      :first_name,
+      :last_name,
+    )
   end
 end
