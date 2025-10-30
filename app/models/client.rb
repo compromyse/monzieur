2943c9ac14976e1c5b31d0c65b64286131e78055
Raghuram Subramani <raghus2247@gmail.com>
2025-10-30 18:37:26 -0400

    client: don't use span unnecessarily

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,11 @@ class Client < ApplicationRecord
   has_many :household_members
 
   accepts_nested_attributes_for :household_members, allow_destroy: true, reject_if: :all_blank
+
+  def last_visit
+    v = visits.last
+    return '-' if v.nil?
+
+    v.created_at.to_date.to_fs(:long) or '-'
+  end
 end
