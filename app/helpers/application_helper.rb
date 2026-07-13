13b7927fc80a17f79d4f7fe6020853e07e54fd50
Raghuram Subramani <raghus2247@gmail.com>
2026-07-12 22:00:24 -0400

    monzieur->pantrie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def get_title
-    Current.pantry.present? ? Current.pantry.name.upcase : "Monzieur"
+    Current.pantry.present? ? Current.pantry.name.upcase : "Pantrie"
   end
 end
